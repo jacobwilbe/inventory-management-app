7fed909d76a540a035c5ca4e2f57e305ddc63990
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-08-01 17:24:24 -0400

    deleted pointless css

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyC5pr1OR5QbQlUnwpaz812RUOGf994hHBw",
   authDomain: "inventory-app-5113a.firebaseapp.com",
@@ -8,7 +9,6 @@ const firebaseConfig = {
   storageBucket: "inventory-app-5113a.appspot.com",
   messagingSenderId: "40066595877",
   appId: "1:40066595877:web:11c0754f6f8c8284dc53cc",
-  measurementId: "G-CVMFBFQGNY"
  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

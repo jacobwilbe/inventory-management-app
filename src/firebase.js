302b774a20a5907bcb9785e5cd91612f86020f38
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-08-01 16:59:11 -0400

    fixed firebase config, and added base ui components and functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyC5pr1OR5QbQlUnwpaz812RUOGf994hHBw",
   authDomain: "inventory-app-5113a.firebaseapp.com",
@@ -14,8 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "40066595877",
   appId: "1:40066595877:web:11c0754f6f8c8284dc53cc",
   measurementId: "G-CVMFBFQGNY"
-};
-
+ };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+export { firestore };

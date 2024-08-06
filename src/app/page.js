4ae5c876e2e733f479426d24589e5eb3ff17631d
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-08-06 00:05:42 -0400

    changed from box to grid layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Box, Stack, Typography, Button, Modal, IconButton, TextField, Card, CardContent, CssBaseline   } from '@mui/material'
+import { Box, Stack, Typography, Button, Modal, IconButton, TextField, Card, CardContent, CssBaseline , Grid  } from '@mui/material'
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import { firestore } from '@/firebase'
 import { Analytics } from "@vercel/analytics/react"
 import Autocomplete from '@mui/material/Autocomplete';
-import { Add as AddIcon, Search as SearchIcon, Edit as EditIcon, Delete as DeleteIcon, Css } from '@mui/icons-material';
+import { Add as AddIcon, Search as SearchIcon, Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
 import CardActions from '@mui/material/CardActions';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 
@@ -50,6 +50,8 @@ const style = {
   gap: 3,
 }
 
+const today = new Date().toISOString().split('T')[0]
+
 
 
 export default function Home() {
@@ -58,16 +60,17 @@ export default function Home() {
   const [open, setAddOpen] = useState(false)
   const [itemName, setItemName] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
-  const [expirationDate, setExpirationDate] = useState('');
+  const [expirationDate, setExpirationDate] = useState(today);
   const [quantity, setQuantity] = useState(1);
   const [editOpen, setEditOpen] = useState(false)
 
   const handleSearch = async () => {
     setItemName(searchQuery)
+    setSearchQuery('')
     setQuantity(inventory.find(item => item.name === searchQuery).quantity)
     setExpirationDate(inventory.find(item => item.name === searchQuery).expirationDate)
     setEditOpen(true)
-    setSearchQuery('')
+    
   }
 
   // fetch inventory data from firestone
@@ -123,7 +126,7 @@ export default function Home() {
     setExpirationDate(expirationDate)
     setEditOpen(true)
   }
-  const handleEditClose = () => setEditOpen(false)
+  const handleEditClose = () => setEditOpen(false)  
 
   const handleDelete = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
@@ -138,188 +141,188 @@ export default function Home() {
 
 
   return (
-
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            padding = {4}
-          >
-            <Analytics mode={'production'} />
-            <Typography variant="h2" textAlign="center" marginBottom={4} color={'white'}>
-              Pantry
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        padding={4}
+      >
+        <Analytics mode={'production'} />
+        <Typography variant="h2" textAlign="center" marginBottom={4} color={'white'}>
+          Pantry Inventory
+        </Typography>
+        <Grid container spacing={2} justifyContent="space-between" alignItems="center" marginBottom={4}>
+          <Grid item xs={12} sm={4}>
+            <Button variant="contained" color="primary" onClick={handleSearch} startIcon={<SearchIcon />} fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Autocomplete
+              options={inventory.map(item => ({ label: item.name }))}
+              getOptionLabel={(option) => option.label}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Search"
+                  variant="outlined"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  sx={{
+                    width: '100%',
+                    '& .MuiOutlinedInput-root': {
+                      backgroundColor: 'white',
+                      color: 'grey',
+                      '& fieldset': {
+                        borderColor: 'white',
+                      },
+                      '&:hover fieldset': {
+                        borderColor: 'white',
+                      },
+                      '&.Mui-focused fieldset': {
+                        borderColor: 'white',
+                      },
+                    },
+                    '& .MuiInputLabel-root': {
+                      color: 'grey',
+                    },
+                  }}
+                />
+              )}
+              onChange={(_, newValue) => {
+                setSearchQuery(newValue ? newValue.label : '');
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Button variant="contained" color="primary" onClick={handleOpenAdd} startIcon={<AddIcon />} fullWidth>
+              Add Item
+            </Button>
+          </Grid>
+        </Grid>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add Item
             </Typography>
-            <Box display="flex" justifyContent="space-between" alignItems="left" marginBottom={4} >
-              <Button variant="contained" color="primary" onClick={handleSearch} startIcon={<SearchIcon />}/>
-              <Autocomplete color='white'
-                options={inventory.map(item => ({ label: item.name }))}
-                getOptionLabel={(option) => option.label}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Search"
-                    variant="outlined"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    sx={{
-                      marginRight: 50,
-                      width: 400,
-                      '& .MuiOutlinedInput-root': {
-                        backgroundColor: 'white',
-                        color: 'grey',
-                        '& fieldset': {
-                          borderColor: 'white',
-                        },
-                        '&:hover fieldset': {
-                          borderColor: 'white',
-                        },
-                        '&.Mui-focused fieldset': {
-                          borderColor: 'white',
-                        },
-                      },
-                      '& .MuiInputLabel-root': {
-                        color: 'grey',
-                      },
-                    }}
-                  />
-                )}
-                onChange={(_, newValue) => {
-                  setSearchQuery(newValue ? newValue.label : '');
-                }}
+            <Stack spacing={2} mt={2}>
+              <TextField
+                label="Name"
+                variant="outlined"
+                fullWidth
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
               />
-              <Button variant="contained" color="primary" onClick={handleOpenAdd} startIcon={<AddIcon />} sx={{marginLeft: 50}}>
-                Add Item
+              <TextField
+                label="Quantity"
+                variant="outlined"
+                fullWidth
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+              />
+              <TextField
+                label="Expiration Date"
+                variant="outlined"
+                fullWidth
+                type="date"
+                InputLabelProps={{ shrink: true }}
+                value={expirationDate}
+                onChange={(e) => setExpirationDate(e.target.value)}
+              />
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  addItem(itemName, quantity, expirationDate);
+                  setQuantity(1);
+                  setExpirationDate(today);
+                  setItemName('');
+                  handleClose();
+                }}
+              >
+                Add
               </Button>
-            </Box>
-            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Add Item
-                </Typography>
-                <Stack spacing={2} mt={2}>
-                  <TextField
-                    label="Name"
-                    variant="outlined"
-                    fullWidth
-                    value={itemName}
-                    onChange={(e) => setItemName(e.target.value)}
-                  />
-                  <TextField
-                    label="Quantity"
-                    variant="outlined"
-                    fullWidth
-                    value={quantity}
-                    onChange={(e) => setQuantity(Number(e.target.value))}
-                  />
-                  <TextField
-                    label="Expiration Date"
-                    variant="outlined"
-                    fullWidth
-                    type="date"
-                    InputLabelProps={{ shrink: true }}
-                    value={expirationDate}
-                    onChange={(e) => setExpirationDate(e.target.value)}
-                  />
-                  <Button
-                    variant="outlined"
-                    onClick={() => {
-                      addItem(itemName, quantity, expirationDate)
-                      setQuantity(1)
-                      setExpirationDate('') 
-                      setItemName('')
-                      handleClose()
-                    }}
-                  >
-                    Add
-                  </Button>
-                </Stack>
-              </Box>
-            </Modal>
-
-            <Modal open={editOpen} onClose={handleEditClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Edit {itemName}
-                </Typography>
-                <Stack spacing={2} mt={2}>
-                  <TextField
-                    label="Quantity"
-                    variant="outlined"
-                    fullWidth
-                    value={quantity}
-                    onChange={(e) => setQuantity(Number(e.target.value))}
-                  />
-                  <TextField
-                    label="Expiration Date"
-                    variant="outlined"
-                    fullWidth
-                    type="date"
-                    InputLabelProps={{ shrink: true }}
-                    value={expirationDate}
-                    onChange={(e) => setExpirationDate(e.target.value)}
-                  />
-                  <Button
-                    variant="outlined"
-                    sx={{ color: 'red' , borderColor: 'red'}}
-                    onClick={() => {
-                      handleDelete(itemName)
-                      setEditOpen(false)
-                    }}
-                  >
-                    Delete
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    onClick={() => {
-                      addItem(itemName, quantity, expirationDate)
-                      setQuantity(1)
-                      setExpirationDate('') 
-                      setItemName('')
-                      handleEditClose()
-                    }}
-                  >
-                    Done
-                  </Button>
-                </Stack>
-              </Box>
-            </Modal>
-
-            <Box display="flex" flexWrap="wrap"  gap={2}  >
-              {inventory.map(({name, quantity, expirationDate}) => (
-                <Card key={name} sx={{ width: 250}} >
-                  <CardHeader 
-                    title={name} 
-                  />
-                  <CardMedia 
-                    component="img"
-                    height="194"
-                    alt= {name} 
-                  />
-                  <CardContent>
-                    <Typography variant="body2" >
-                      <strong>Expiration Date:</strong> <br /> {expirationDate}
-                    </Typography>
-                    <Typography variant="body2" >
-                      <strong>Quantity:</strong> {quantity}
-                    </Typography>
-                    </CardContent>
-                    <CardActions 
-                      disableSpacing
-                    >
-                      <IconButton onClick={handleOpenEdit(name, quantity, expirationDate)}>
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton  onClick={() => removeItem(name, quantity, expirationDate)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </CardActions>
-                </Card>
-              ))}
-            </Box>
+            </Stack>
           </Box>
-      </ThemeProvider>
+        </Modal>
+
+        <Modal open={editOpen} onClose={handleEditClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Edit {itemName}
+            </Typography>
+            <Stack spacing={2} mt={2}>
+              <TextField
+                label="Quantity"
+                variant="outlined"
+                fullWidth
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+              />
+              <TextField
+                label="Expiration Date"
+                variant="outlined"
+                fullWidth
+                type="date"
+                InputLabelProps={{ shrink: true }}
+                value={expirationDate}
+                onChange={(e) => setExpirationDate(e.target.value)}
+              />
+              <Button
+                variant="outlined"
+                sx={{ color: 'red', borderColor: 'red' }}
+                onClick={() => {
+                  handleDelete(itemName);
+                  setEditOpen(false);
+                }}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  addItem(itemName, quantity, expirationDate);
+                  setQuantity(1);
+                  setExpirationDate('');
+                  setItemName('');
+                  handleEditClose();
+                }}
+              >
+                Done
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+
+        <Grid container spacing={2} justifyContent="flex-start" alignItems="center" marginLeft={1}>
+          {inventory.map(({ name, quantity, expirationDate }) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={name}>
+              <Card sx={{ width: '75%' }}>
+                <CardHeader title={name} />
+                <CardMedia alt={name} component="img" height="194" />
+                <CardContent>
+                  <Typography variant="body2">
+                    <strong>Expiration Date:</strong> <br /> {expirationDate}
+                  </Typography>
+                  <Typography variant="body2">
+                    <strong>Quantity:</strong> {quantity}
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton onClick={handleOpenEdit(name, quantity, expirationDate)}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => removeItem(name, quantity, expirationDate)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </ThemeProvider>
+
+      
   )
 }
